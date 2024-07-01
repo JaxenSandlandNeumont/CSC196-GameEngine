b636e3c26a5bb338b1dd184bc24115547ddc8fc8
JaxenSandlandNeumont <jsandland@student.neumont.edu>
2024-07-01 13:53:54 -0600

    Added Renderer

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,62 +1,86 @@
+#include "Renderer.h"
+#include "Vector2.h"
+
 #include <SDL.h>
 #include <iostream>
+#include <cstdlib>
+#include <vector>
 
 int main(int argc, char* argv[])
 {
-	// initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	
+
+	Renderer renderer;
+	renderer.Initialize();
+	renderer.CreateWindow("Window", 800, 600);
+
+	Vector2 v1{ 400, 500 };
+	Vector2 v2{ 700, 500 };
+
+	std::vector<Vector2> points;
+		for (int i = 0; i < 100; i++)
+		{
+			points.push_back(Vector2{ rand() % 800, rand() % 600 });
+		}
+
+	while (true)
 	{
-		std::cerr << "Error initializing SDL: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+		
+		//input
+		//update
+		//draw
 
-	// create window
-	// returns pointer to window if successful or nullptr if failed
-	SDL_Window* window = SDL_CreateWindow("Game Engine",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		800, 600,
-		SDL_WINDOW_SHOWN);
-	if (window == nullptr)
-	{
-		std::cerr << "Error creating SDL window: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
+		//update
+		Vector2 speed(0.1f, -0.1f);
+		for (Vector2& point : points )
+		{
+			point = point + 0.002f;
+		}
 
-	// create renderer
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+		renderer.SetColor(0, 0, 0, 0);
+		renderer.BeginFrame();
 
-	while (false)
-	{
-		// clear screen
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-		SDL_RenderClear(renderer);
+		renderer.DrawLine(400, 100, 100, 500);
+		renderer.DrawLine(100, 500, 700, 500);
+		renderer.DrawLine(700, 500, 400, 100);
+		renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);
 
-		// draw line
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-		SDL_RenderDrawLine(renderer, 400, 100, 100, 500);
-		SDL_RenderDrawLine(renderer, 100, 500, 700, 500);
-		SDL_RenderDrawLine(renderer, 700, 500, 400, 100);
+		for (int i = 0; i < points.size() - 1; i++)
+		{
+			renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+		}
 
-		// show screen
-		SDL_RenderPresent(renderer);
+		renderer.EndFrame();
 	}
 
 	const int MAXWIDTH = 800;
-	while (true)
+	while (false)
 	{
-		int value = rand() % 1000; // generate a number between 0-999
-		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
-		SDL_RenderClear(renderer);
-		
-		for (int i = 0; i < value; i++)
-		{
-			SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 0); // create random color
-			SDL_RenderDrawLine(renderer, rand() % MAXWIDTH, rand() % MAXWIDTH, rand() % MAXWIDTH, rand() % MAXWIDTH);
-		}
-		SDL_RenderPresent(renderer);
+		//int value = rand() % 1000; // generate a number between 0-999
+		//SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+		//SDL_RenderClear(renderer);
+		//
+		//for (int i = 0; i < value; i++)
+		//{
+		//	SDL_SetRenderDrawColor(renderer, rand() % 256, rand() % 256, rand() % 256, 0); // create random color
+		//	SDL_RenderDrawLine(renderer, rand() % MAXWIDTH, rand() % MAXWIDTH, rand() % MAXWIDTH, rand() % MAXWIDTH);
+		//}
+		//SDL_RenderPresent(renderer);
 		
 	}
 
 	return 0;
+
+	
+//	+ create a std::vector of Vector2 outside the main loop, these are the points
+//main loop (while):
+//  // input
+//  + check if mouse button is down and previous mouse button is up (mouse pressed)
+//    + add (push_back) mouse position onto points vector
+//  // render
+//  // use for loop to render lines (point[i] -> point[i + 1])
+//  + for (int i = 0; points.size() > 1 && i < points.size() - 1; i++)
+//    + set color (have fun and set random color :))
+//    + renderer draw line (points[i].x, points[i].y, points[i + 1].x, points[i + 1].y)
+
 }
