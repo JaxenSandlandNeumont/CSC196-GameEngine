e061b87c54607b93af8196082ac8e74865231292
JaxenSandlandNeumont <jsandland@student.neumont.edu>
2024-07-02 10:15:33 -0600

    Added input system

---
+++ Engine/Source/Input.cpp
@@ -0,0 +1,44 @@
+#include "Input.h"
+#include <SDL.h>
+
+bool Input::Initialize()
+{
+	int numKeys;
+	const uint8_t* keyBoardState = SDL_GetKeyboardState(&numKeys);
+
+	m_keyboardState.resize(numKeys);
+
+	// Copy curent keyboard state into our keyboard state
+	std::copy(keyBoardState, keyBoardState + numKeys, m_keyboardState.begin());
+	m_prevKeyboardState = m_keyboardState;
+
+	return true;
+}
+
+void Input::Shutdown()
+{
+
+}
+
+void Input::Update()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	// keyboard input
+	m_prevKeyboardState = m_keyboardState;
+	const uint8_t* keyBoardState = SDL_GetKeyboardState(nullptr);
+	std::copy(keyBoardState, keyBoardState + m_keyboardState.size(), m_keyboardState.begin());
+
+	int x, y;
+	uint32_t buttonState = SDL_GetMouseState(&x, &y);
+
+	m_mousePosition.x = (float)x;
+	m_mousePosition.y = (float)y;
+
+	m_prevMouseButtonState = m_mouseButtonState;
+
+	m_mouseButtonState[0] = buttonState & SDL_BUTTON_LMASK;
+	m_mouseButtonState[1] = buttonState & SDL_BUTTON_MMASK;
+	m_mouseButtonState[2] = buttonState & SDL_BUTTON_RMASK;
+}
--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -1,4 +1,5 @@
 #include "Renderer.h"
+#include "Input.h"
 #include "Vector2.h"
 
 #include <SDL.h>
@@ -14,39 +15,56 @@ int main(int argc, char* argv[])
 	renderer.Initialize();
 	renderer.CreateWindow("Window", 800, 600);
 
+	Input input;
+	input.Initialize();
+
 	Vector2 v1{ 400, 500 };
 	Vector2 v2{ 700, 500 };
 
 	std::vector<Vector2> points;
-		for (int i = 0; i < 100; i++)
-		{
-			points.push_back(Vector2{ rand() % 800, rand() % 600 });
-		}
-
-	while (true)
+	//for (int i = 0; i < 100; i++)
+	//{
+	//	points.push_back(Vector2{ rand() % 800, rand() % 600 });
+	//}
+	bool quit = false;
+	while (!quit)
 	{
 		
 		//input
 		//update
 		//draw
 
-		//update
-		Vector2 speed(0.1f, -0.1f);
-		for (Vector2& point : points )
+		//INPUT
+		input.Update();
+
+		if (input.GetKeyDown(SDL_SCANCODE_ESCAPE))
 		{
-			point = point + 0.002f;
+			quit = true;
+		}
+
+		//UPDATE
+		Vector2 mousePosition = input.GetMousePosition();
+		if (input.GetMouseButtonDown(0) && !input.GetPreviousMouseButtonDown(0))
+		{
+			points.push_back(mousePosition);
+		}
+
+		if (input.GetMouseButtonDown(0) && input.GetPreviousMouseButtonDown(0))
+		{
+			float distance = (points.back() - mousePosition).Length();
+			if (distance > 10)
+			{
+				points.push_back(mousePosition);
+			}
 		}
 
 		renderer.SetColor(0, 0, 0, 0);
 		renderer.BeginFrame();
 
-		renderer.DrawLine(400, 100, 100, 500);
-		renderer.DrawLine(100, 500, 700, 500);
-		renderer.DrawLine(700, 500, 400, 100);
-		renderer.DrawLine(v1.x, v1.y, v2.x, v2.y);
-
-		for (int i = 0; i < points.size() - 1; i++)
+		renderer.SetColor(500, 0, 500, 0);
+		for (int i = 0; points.size() > 1 && i < points.size() - 1; i++)
 		{
+
 			renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
 		}
 
